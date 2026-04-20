bee13a07e3e23deedd7810c89921be4255dd7bad
Mxfalconmx1 <mariofalcon030901@gmail.com>
2026-04-19 19:18:25 -0600

    Update utils.py

--- core/utils.py
+++ core/utils.py
@@ -1,5 +1,14 @@
 import json
+from datetime import datetime
 
-def save_result(data):
+def save_result(target, service, username, password):
+    data = {
+        "target": target,
+        "service": service,
+        "username": username,
+        "password": password,
+        "timestamp": str(datetime.now())
+    }
+
     with open("output/results.json", "w") as f:
         json.dump(data, f, indent=4)
