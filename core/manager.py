8b343d657de41ced3f5aee71a5de45f22a1dc714
Mxfalconmx1 <mariofalcon030901@gmail.com>
2026-04-19 19:15:14 -0600

    Refactor attack handling to use plugins

--- core/manager.py
+++ core/manager.py
@@ -1,17 +1,14 @@
-from modules import ssh, ftp, http
+from core.plugin_loader import load_plugins
 
 class AttackManager:
     def __init__(self, target, service, username):
         self.target = target
         self.service = service
         self.username = username
+        self.plugins = load_plugins()
 
     def run(self):
-        if self.service == "ssh":
-            ssh.attack(self.target, self.username)
-        elif self.service == "ftp":
-            ftp.attack(self.target, self.username)
-        elif self.service == "http":
-            http.attack(self.target, self.username)
+        if self.service in self.plugins:
+            self.plugins[self.service].attack(self.target, self.username)
         else:
-            print("Servicio no soportado")
+            print("[!] Servicio no soportado")
