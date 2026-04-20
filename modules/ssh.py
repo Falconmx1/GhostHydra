844191d1e64bd559922c881f1e8fbbca8ef7d54f
Mxfalconmx1 <mariofalcon030901@gmail.com>
2026-04-19 19:20:16 -0600

    Update ssh.py

--- modules/ssh.py
+++ modules/ssh.py
@@ -1,14 +1,26 @@
+from core.engine import run_threads
+from core.wordlist import generate_wordlist
+from core.stealth import random_delay
+from core.utils import save_result
+
 def attack(target, username):
-    print(f"[+] Simulando ataque SSH en {target} con usuario {username}")
+    print(f"[+] Ataque SSH en {target}")
 
-    passwords = ["1234", "admin", "password"]
+    passwords = generate_wordlist(username)
+    found = {"status": False}
 
-    for pwd in passwords:
-        print(f"[*] Probando: {pwd}")
-
-        # Simulación
-        if pwd == "admin":
-            print(f"[SUCCESS] Password encontrada: {pwd}")
+    def worker(pwd):
+        if found["status"]:
             return
 
-    print("[-] No se encontró password")
+        random_delay()
+
+        print(f"[*] Probando {pwd}")
+
+        # Simulación controlada
+        if pwd == username + "123":
+            print(f"[SUCCESS] {pwd}")
+            save_result(target, "ssh", username, pwd)
+            found["status"] = True
+
+    run_threads(passwords, worker, threads=5)
